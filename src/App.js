47eca0f8c8bb4a2ad2c0902b847068e2318f7256
Elena Kichinskaia <nitress@gmail.com>
2021-06-16 12:19:06 +0300

    add suspense and lazy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { Suspense, lazy } from "react";
 import { Route, NavLink, Switch } from "react-router-dom";
-import HomeView from "./view/HomeView";
-import MoviesView from "./view/MoviesView";
+// import HomeView from "./view/HomeView";
+// import MoviesView from "./view/MoviesView";
 import NotFoundView from "./view/NotFoundView";
-import MovieDetailsView from "./view/MovieDetailsView";
+// import MovieDetailsView from "./view/MovieDetailsView";
 import routes from "./routes";
 import AppBar from "./components/AppBar";
+
+const HomeView = lazy(() =>
+  import("./view/HomeView" /* webpackChunkName: "home-view" */)
+);
+const MoviesView = lazy(() =>
+  import("./view/MoviesView" /* webpackChunkName: "movies-view" */)
+);
+const MovieDetailsView = lazy(() =>
+  import("./view/MovieDetailsView" /* webpackChunkName: "book-details-view" */)
+);
+// const NotFoundView = lazy(() => import("./view/NotFoundView"));
 
 // const linkStyles = {
 //   base: {
@@ -39,12 +51,14 @@ const App = () => (
         </NavLink>
       </li>
     </ul> */}
-    <Switch>
-      <Route exact path={routes.home} component={HomeView} />
-      <Route path={routes.movieDetails} component={MovieDetailsView}></Route>
-      <Route exact path={routes.movie} component={MoviesView} />
-      <Route component={NotFoundView} />
-    </Switch>
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Switch>
+        <Route exact path={routes.home} component={HomeView} />
+        <Route path={routes.movieDetails} component={MovieDetailsView}></Route>
+        <Route exact path={routes.movie} component={MoviesView} />
+        <Route component={NotFoundView} />
+      </Switch>
+    </Suspense>
   </>
 );
 
